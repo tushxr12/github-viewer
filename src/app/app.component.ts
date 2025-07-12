d87c4faf49c44b414d2f6572ad80f32288b58e02
Tushar Sharma <tusharforcourses@gmail.com>
2025-07-12 12:48:25 +0530

    add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [FormsModule, CommonModule, HttpClientModule, HeaderComponent]
+  imports: [FormsModule, CommonModule, HttpClientModule, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   username: string = '';
